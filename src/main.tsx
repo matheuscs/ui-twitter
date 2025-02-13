496b74a8fac9655530f1799d94cf22c2e04fbaae
Matheus Silva <matheuscarvas@gmail.com>
2025-02-13 11:07:31 -0300

    implementing RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
 import { Sidebar } from './components/Sidebar'
-import { Timeline } from './pages/Timeline'
+import { router } from './routes'
 
 import './global.css'
+import { RouterProvider } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <div className='layout'>
       <Sidebar />
       <div className="content">
-      <Timeline />
+        <RouterProvider router={router} />
       </div>
     </div>
   </StrictMode>
---
+++ src/routes.tsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Timeline } from './pages/Timeline'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Timeline />
+  }
+])
